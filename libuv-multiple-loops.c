5631aa64832087fb8a3ddfd440eaa7bb1815bf23
Lei <lynnl.yet@gmail.com>
2020-01-20 22:57:15 +0800

    Reduced minimal reproducible program

--- libuv-multiple-loops.c
+++ libuv-multiple-loops.c
@@ -2,8 +2,6 @@
  * libuv multiple loops + thread communication example.
  * Written by Kristian Evensen <kristian.evensen@gmail.com>
  *
- * Slightly modified by leiless
- *
  * see:
  *  https://github.com/kristrev/libuv-multiple-loops
  *  http://nikhilm.github.io/uvbook/multiple.html
@@ -13,69 +11,18 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 #include <assert.h>
-#include <stdarg.h>
 
 #include <unistd.h>
 #include <sys/syscall.h>
 
 #include <uv.h>
 
-#define UNUSED(e, ...)      (void) ((void) (e), ##__VA_ARGS__)
-
 #define LOG(fmt, ...)       \
     (void) printf("[tid: %#lx] " fmt "\n", syscall(SYS_gettid), ##__VA_ARGS__)
 
-/* Macro taken from macOS/Frameworks/Kernel/sys/cdefs.h */
-#define __printflike(fmtarg, firstvararg) \
-                __attribute__((__format__(__printf__, fmtarg, firstvararg)))
-
-static void __assertf(int, const char *, ...) __printflike(2, 3);
-
-/**
- * Formatted version of assert()
- *
- * @param expr  Expression to assert with
- * @param fmt   Format string when assertion failed
- * @param ...   Format string arguments
- */
-static void __assertf(int expr, const char *fmt, ...)
-{
-    int n;
-    va_list ap;
-
-    if (!expr) {
-        va_start(ap, fmt);
-        n = vfprintf(stderr, fmt, ap);
-        assert(n > 0);  /* Should never fail! */
-        va_end(ap);
-
-        abort();
-    }
-}
-
-#define assertf(e, fmt, ...)                                        \
-    __assertf(!!(e), "Assert (%s) failed: " fmt "  %s@%s()#%d\n",   \
-                #e, ##__VA_ARGS__, __BASE_FILE__, __func__, __LINE__)
-
-#define panicf(fmt, ...)            assertf(0, fmt, ##__VA_ARGS__)
-
-#define assert_nonnull(ptr)         assertf(ptr != NULL, "")
-
-#define __assert_cmp(v1, v2, fmt, op)   \
-    assertf((v1) op (v2), "left: " fmt " right: " fmt, (v1), (typeof(v1)) (v2))
-
-#define assert_eq(v1, v2, fmt)   __assert_cmp(v1, v2, fmt, ==)
-#define assert_ne(v1, v2, fmt)   __assert_cmp(v1, v2, fmt, !=)
-#define assert_le(v1, v2, fmt)   __assert_cmp(v1, v2, fmt, <=)
-#define assert_ge(v1, v2, fmt)   __assert_cmp(v1, v2, fmt, >=)
-#define assert_lt(v1, v2, fmt)   __assert_cmp(v1, v2, fmt, <)
-#define assert_gt(v1, v2, fmt)   __assert_cmp(v1, v2, fmt, >)
-
-/* bclr stands for byte clear */
-#define bclr(ptr, size)          (void) memset(ptr, 0, size)
-#define bclr_sizeof(ptr)         bclr(ptr, sizeof(*(ptr)))
+#define assert_nonnull(ptr)         assert(ptr != NULL)
+#define assert_eq(v1, v2)           assert(v1 == v2)
 
 typedef struct {
     uv_loop_t loop;
@@ -97,16 +44,10 @@ static void timer_cb(uv_timer_t *handle)
     LOG("Timer expired, notifying other thread");
 
     uv_work_t req;
-    bclr_sizeof(&req);
+    (void) memset(&req, 0, sizeof(req));
     req.data = NULL;
     e = uv_queue_work(&la->loop, &req, work_cb, NULL);
-    assert_eq(e, 0, "%d");
-
-#if 0
-    /* Notify the other thread */
-    e = uv_async_send(&la->async);
-    assert_eq(e, 0, "%d");
-#endif
+    assert_eq(e, 0);
 }
 
 static void thread_entry(void *data)
@@ -115,64 +56,55 @@ static void thread_entry(void *data)
 
     uv_loop_t *thread_loop = (uv_loop_t *) data;
     int e= uv_run(thread_loop, UV_RUN_DEFAULT);
-    assert_eq(e, 0, "%d");
+    assert_eq(e, 0);
 
     LOG("(Consumer event loop done)");
-}
-
-static void async_cb(uv_async_t *handle)
-{
-    LOG("(Got notify from the other thread  fd: %d data: %p)\n",
-            handle->loop->backend_fd, handle->data);
 }
 
 int main(void)
 {
     int e;
 
-#ifdef DEBUG
     // see: https://stackoverflow.com/questions/35239938/should-i-set-stdout-and-stdin-to-be-unbuffered-in-c
     e = setvbuf(stdout, NULL, _IONBF, 0);
-    assert_eq(e, 0, "%d");
+    assert_eq(e, 0);
     LOG("Set stdout unbuffered");
-#endif
 
     loop_async_t la;
 
     e = uv_loop_init(&la.loop);
-    assert_eq(e, 0, "%d");
+    assert_eq(e, 0);
     LOG("thread loop fd: %d", la.loop.backend_fd);
 
-    bclr_sizeof(&la.async);
-    //e = uv_async_init(&la.loop, &la.async, async_cb);
+    (void) memset(&la.async, 0, sizeof(la.async));
     e = uv_async_init(&la.loop, &la.async, NULL);
-    assert_eq(e, 0, "%d");
+    assert_eq(e, 0);
 
     uv_thread_t thread;
     e = uv_thread_create(&thread, thread_entry, &la.loop);
-    assert_eq(e, 0, "%d");
+    assert_eq(e, 0);
 
     /* Main thread will run default loop */
     uv_loop_t *main_loop = uv_default_loop();
     uv_timer_t timer_req;
     e = uv_timer_init(main_loop, &timer_req);
-    assert_eq(e, 0, "%d");
+    assert_eq(e, 0);
     LOG("main loop fd: %d", main_loop->backend_fd);
 
     /* Timer callback needs async so it knows where to send messages */
     timer_req.data = &la;
     e = uv_timer_start(&timer_req, timer_cb, 0, 1000);
-    assert_eq(e, 0, "%d");
+    assert_eq(e, 0);
 
     LOG("Starting main loop\n");
     e = uv_run(main_loop, UV_RUN_DEFAULT);
-    assert_eq(e, 0, "%d");
+    assert_eq(e, 0);
 
     e = uv_thread_join(&thread);
-    assert_eq(e, 0, "%d");
+    assert_eq(e, 0);
 
     e = uv_loop_close(main_loop);
-    assert_eq(e, 0, "%d");
+    assert_eq(e, 0);
 
     return 0;
 }
